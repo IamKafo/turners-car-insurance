6fbe731f38a010e09c4863f6b15d3cf3b164e333
DESKTOP-IEN7UUP\64211 <frederickhy001@gmail.com>
2022-05-23 11:38:13 +1200

    modify to pass tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Footer from './Components/Footer/Footer';
+//import Footer from './Components/Footer/Footer';
 //import HomePage from './Components/HomePage/HomePage';
-import NavBar from './Components/NavBar/NavBar';
+//import NavBar from './Components/NavBar/NavBar';
 import Page2 from './Components/page2/page2.js'
 
 function App() {
