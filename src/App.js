0aa6d24e842bc4fd0bb88af820ca49f37eed2bbe
DESKTOP-IEN7UUP\64211 <frederickhy001@gmail.com>
2022-05-23 23:11:48 +1200

    Modified footer, moved existing quote button into position, started ReviewPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import HomePage from './Components/HomePage/HomePage';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GetQuote from './Components/GetQuote/GetQuote';
+import ReviewPage from './Components/ReviewPage/ReviewPage';
+
 
 
 function App() {
@@ -12,9 +14,9 @@ function App() {
           <Routes>
             <Route exact path="/"  element={<HomePage />} />
             <Route  path="/getquote" element={<GetQuote />} />
+            <Route path="/reviewpage" element={<ReviewPage/>} />
           </Routes>
         </Router>
-
       </>
 
   );
