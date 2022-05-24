f73af232338d82e32b67794d479d63813609ca68
Kafo Tila <kafolautila@.com>
2022-05-24 18:25:19 +1200

    updated the app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 
 import HomePage from './Components/HomePage/HomePage';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import GetQuote from './Components/GetQuote/GetQuote';
 import ExQuote from './Components/ExQuote/ExQuote';
 import ChoosePolicy from './Components/ChoosePolicy/ChoosePolicy';
 //import ReviewPage from './Components/ReviewPage/ReviewPage';
@@ -17,8 +16,7 @@ function App() {
         <Router>
           <Routes>
             <Route exact path="/"  element={<HomePage />} />
-            <Route  path="/getquote" element={<GetQuote />} />
-            <Route  path="/existingquote" element={<ExQuote />} 
+            <Route  path="/existingquote" element={<ExQuote />} />
             <Route  path="/choosepolicy" element={<ChoosePolicy />} />
             <Route path="/newquote" element={<NewQuotePage/>} />
 
