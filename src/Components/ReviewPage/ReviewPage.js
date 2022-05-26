8609b462e330016aa033eebfe8d47087d3987a55
Kafo Tila <kafolautila@.com>
2022-05-27 05:36:15 +1200

    modified the review page

--- src/Components/ReviewPage/ReviewPage.js
+++ src/Components/ReviewPage/ReviewPage.js
@@ -31,6 +31,11 @@ function calculateValue (value){
 const ReviewPage = () => {
 
     const [value, setValue] = useState(62.68)
+    const [email, setEmail] = useState(false)
+
+    const handleClick = () => {
+        setEmail(email => !setEmail)
+    }
 
     const handleChange = (e,newValue) => {
         if (typeof newValue === 'number') {
@@ -228,10 +233,14 @@ const ReviewPage = () => {
                 </div>
             </div>
             <div className="review-button-container">
+                <div className="need-to-think-about-it"> Need time to think about it?</div>
             <div className="review-button">
                 <button className='go-back-to-edit-btn'><b><NavLink className="NavLink" to='/'>Go back edit details</NavLink></b></button>
-                <button className='email-my-quote'><b>Email my quote</b></button>
+                <button onClick={handleClick} className='email-my-quote'><b>Email my quote</b>
+
+                </button>
                 <button className='go-to-payment'><b><NavLink className="NavLink" to='/'>Go to payment</NavLink></b></button>
+                
             </div>
         </div>
         <Footer/>
