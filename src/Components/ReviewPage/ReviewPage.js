9fbe9c49b176249e912c62698381bf1fe2878664
Kafo Tila <kafolautila@.com>
2022-05-27 06:10:21 +1200

    fix the issue

--- src/Components/ReviewPage/ReviewPage.js
+++ src/Components/ReviewPage/ReviewPage.js
@@ -236,7 +236,7 @@ const ReviewPage = () => {
                 <div className="need-to-think-about-it"> Need time to think about it?</div>
             <div className="review-button">
                 <button className='go-back-to-edit-btn'><b><NavLink className="NavLink" to='/existingdetailspage'>Go back edit details</NavLink></b></button>
-                <button className={handleClick}'email-my-quote'><b>Email my quote</b></button>
+                <button onClick={handleClick} className='email-my-quote'><b>Email my quote</b></button>
                 <button className='go-to-payment'><b><NavLink className="NavLink" to='/buypolicy'>Go to payment</NavLink></b></button>
 
             </div>
